8af232be31bc8d2bcc7cef55d24abc915535426f
LassaL <lassal@seznam.cz>
2019-01-29 21:39:39 +0530

    initial drag and drop show

--- src/app/drag-and-drop/containers/drag-and-drop/drag-and-drop.component.ts
+++ src/app/drag-and-drop/containers/drag-and-drop/drag-and-drop.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CdkDrag, CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-drag-and-drop',
   templateUrl: './drag-and-drop.component.html',
   styleUrls: ['./drag-and-drop.component.css']
 })
-export class DragAndDropComponent implements OnInit {
+export class DragAndDropComponent {
 
-  constructor() { }
+  items = ['Zero', 'One', 'Two', 'Three'];
+  items2 = ['Zero', 'One', 'Two', 'Three'];
+  items3 = ['Eleven'];
 
-  ngOnInit() {
+  onDrop(event: CdkDragDrop<string[]>) {
+    console.log('onDrop: ', event);
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
+
+  addToList(event: CdkDragDrop<string[]>) {
+    console.log('addToList: ', event);
+    if (event.previousContainer === event.container) {
+      console.log('moveItemInArray');
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      console.log('transferArrayItem');
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+
+  }
+
+  isAllowed() {
+    return false;
   }
 
 }
